1e9545e9a06e24d7bd7b4935332a5798e3975165
Matthew Laulusa <mlaulusa@users.noreply.github.com>
2015-07-14 13:48:46 -0600

    asthetic changes, bootstrap

--- app/partial/inbox/inbox.js
+++ app/partial/inbox/inbox.js
@@ -10,12 +10,34 @@ angular.module('kleen.inbox', ['ngRoute'])
 	});
 }])
 .controller('InboxCtrl', ['$scope', '$http', function($scope, $http){
+	$scope.current = {};
+	$scope.messages = [];
+
 	$http.get('/partial/inbox/work-orders.json').then(function(res){
 		$scope.tickets = res.data;
+		var message = $scope.tickets[0];
+		$scope.messages.push({
+			"author": message.author,
+			"message": message.message,
+			"date_submitted": message.date_submitted
+		});
+		for (var i = 0; i < $scope.tickets[0].replies.length; i++) {
+			$scope.messages.push($scope.tickets[0].replies[i]);
+		}
+		$scope.current = tickets[0];
 	});
-	$scope.focus = {};
+
 	$scope.ticketFocus = function (ticket) {
-		$scope.focus = ticket;
+		$scope.messages = [];
+		$scope.messages.push({
+			"author": ticket.author,
+			"message": ticket.message,
+			"date_submitted": ticket.date_submitted
+		});
+		for (var i = 0; i < $scope.tickets[0].replies.length; i++) {
+			$scope.messages.push(ticket.replies[i]);
+		}
+		$scope.current = ticket;
 	};
 
 }]);
